b5de89ee74d0539e3ca3b0bec2a67f341828232f
engineer.y.o <engineer.y.o3911@gmail.com>
2020-01-29 07:46:22 +0000

    change before_action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
-  before_action :require_uer_logged_in, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :require_uer_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = current_user.tasks.order(id: :desc)
