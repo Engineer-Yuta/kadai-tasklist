1f081a1e816ffa1d8dd1db0cc4eeeb5b823e7f02
engineer.y.o <engineer.y.o3911@gmail.com>
2020-01-29 06:42:03 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,9 @@
 class TasksController < ApplicationController
-  before_action :require_uer_logged_in, only: [:show, :new, :create, :edit, :update, :destroy]
+  before_action :require_uer_logged_in, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.order(id: :desc)
   end
   
   def show
@@ -10,11 +11,11 @@ class TasksController < ApplicationController
   end
   
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "Task が正常に投稿されました"
@@ -54,5 +55,12 @@ class TasksController < ApplicationController
   # Strong Prameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task =current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
